00a00e8bd11b6e27425fa19c6414d599fc284d08
A.J. Hawkey <aaronhawkey48@gmail.com>
2019-10-20 23:57:26 -0500

    Added docker config for redis

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var hbs = require('hbs');
 var app = express();
-var server = require('http').Server(app)
-var io = require('socket.io').listen(server)
+var redis = require('socket.io-redis');
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+io.adapter(redis({ host: process.env.REDIS_ENDPOINT, port: 6379 }));
 
 app.set('view engine', 'hbs');
 
-server.listen(process.env.PORT || 3000, ()=> console.log(`App listening on port ${process.env.PORT || 3000}`))
+server.listen(process.env.PORT || 3000, () => console.log(`App listening on port ${process.env.PORT || 3000}`))
 
 app.get('/', (req, res) => {
     res.render('index', {url: process.env.URL || 'localhost:3000'});
