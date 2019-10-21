6fee534bc56c5e557adf1f51225c3deaa8019c2f
Aaron Hawkey <aaron@highschoolesportsleague.com>
2019-10-21 17:21:29 -0500

    Added queue events and basic cron.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var redis2 = require('redis');
 var hbs = require('hbs');
+var bodyParser = require('body-parser');
 var app = express();
 var redis = require('socket.io-redis');
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+var cron = require('node-cron');
 
-io.adapter(redis({ host: process.env.REDIS_ENDPOINT, port: 6379 }));
+
+app.use(bodyParser.json());
+
+if(process.env.REDIS_ENDPOINT){
+    io.adapter(redis({ host: process.env.REDIS_ENDPOINT, port: 6379 }));
+    var rClient = redis2.createClient(`redis://${process.env.REDIS_ENDPOINT}`);
+    console.log('Connection to Redis made.');
+}
 
 app.set('view engine', 'hbs');
 
@@ -13,6 +23,11 @@ server.listen(process.env.PORT || 3000, () => console.log(`App listening on port
 
 app.get('/', (req, res) => {
     res.render('index', {url: process.env.URL || 'localhost:3000'});
+});
+
+app.post('/match-ready/:partyId', (req, res) => {
+    io.in(req.params.partyId).emit('match-ready', {event: 'match ready'});
+    res.status(200).send({status: 'ok'});
 });
 
 io.on('connection', (socket) => {
@@ -31,4 +46,22 @@ io.on('connection', (socket) => {
         io.sockets.in(party).emit('party-join-success-' + socket.id, {response: 'successs'});
     });
 
+    socket.on('enter-queue', ({ partyId, game }) => {
+        console.log('In Queue Now');
+        console.log(`PartyId: ${partyId}`);
+        console.log(`Game: ${game}`);
+        rClient.set(`queue:${game}:party:${partyId}`, 'inQueue');
+
+    });
+
 });
+
+
+if(process.env.CRON_STATUS == 'true') {
+    console.log('in if');
+    cron.schedule("*/5 * * * * *", () => {
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        console.log(time);
+      });
+}
